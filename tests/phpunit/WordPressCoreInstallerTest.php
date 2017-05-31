583c8e2731d96cd0cd021c8a81752d17f008d769
John P Bloch <johnpbloch@gmail.com>
2017-05-31 10:42:54 -0500

    GH-1 Add a test for default install dir

--- tests/phpunit/WordPressCoreInstallerTest.php
+++ tests/phpunit/WordPressCoreInstallerTest.php
@@ -5,6 +5,7 @@ namespace Tests\JohnPBloch\Composer\phpunit;
 use Composer\Composer;
 use Composer\Config;
 use Composer\IO\NullIO;
+use Composer\Package\Package;
 use johnpbloch\Composer\WordPressCoreInstaller;
 use PHPUnit\Framework\TestCase;
 
@@ -17,6 +18,15 @@ class WordPressCoreInstallerTest extends TestCase {
 
 		$this->assertTrue( $installer->supports( 'wordpress-core' ) );
 		$this->assertFalse( $installer->supports( 'not-wordpress-core' ) );
+	}
+
+	public function testDefaultInstallDir() {
+		$composer = new Composer();
+		$composer->setConfig( new Config() );
+		$installer = new WordPressCoreInstaller( new NullIO(), $composer );
+		$package   = new Package( 'johnpbloch/test-package', '1.0.0.0', '1.0.0' );
+
+		$this->assertEquals( 'wordpress', $installer->getInstallPath( $package ) );
 	}
 
 }
